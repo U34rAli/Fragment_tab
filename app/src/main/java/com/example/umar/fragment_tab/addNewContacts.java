a07d764d1196de15e8c71d2e13a30fb30d86ce90
Umar <uali10772@gmail.com>
2017-07-24 18:23:06 +0500

    addNewContacts

---
+++ app/src/main/java/com/example/umar/fragment_tab/addNewContacts.java
@@ -0,0 +1,13 @@
+package com.example.umar.fragment_tab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addNewContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contacts);
+    }
+}
